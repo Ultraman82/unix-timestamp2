b9255be91689f9fa4ee23f07fdc7437d8566763c
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2017-03-04 02:59:04 +0000

    Implement API

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.render('index', {basedir: __dirname});
 });
 
+app.get('/api/', function (req, res) {
+  var os = req.header('user-agent').split(')')[0].split('(')[1];
+  var language = req.header('accept-language').split(',')[0];
+  var ip = req.header('x-forwarded-for');
+  
+  res.send({
+    os: os,
+    language: language,
+    ip: ip
+  });
+});
+
 app.listen(process.env.PORT || 8080, function () {
   console.log('API is listening!');
 });
