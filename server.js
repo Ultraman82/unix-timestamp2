7f2f2353b769ad6e4f0f67ec5505e6ac0c38b310
Ultraman82 <exelcior99@gmail.com>
2017-11-02 19:26:13 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
-app.set('views', 'templates');
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.render('index', {basedir: __dirname});
+  res.render('index');
 });
 
-app.get('/whoami/', function (req, res) {
+app.get('/whoami', function (req, res) {
   var os = req.header('user-agent').split(')')[0].split('(')[1];
   var language = req.header('accept-language').split(',')[0];
-  var ip = req.header('x-forwarded-for');
-  
-  res.send({
+  var ip = req.header('x-forwarded-for').split(',')[0];
+  var data = {
     os: os,
     language: language,
     ip: ip
-  });
+  };  
+  res.render('whoami', {data:data});
+  
 });
 
 app.listen(process.env.PORT || 8080, function () {
